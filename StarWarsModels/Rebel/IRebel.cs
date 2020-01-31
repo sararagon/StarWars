191415f12419c2b0da294d97981e7a46b2410aa4
Sara Porcar Palacios <sararagon.5@live.com>
2020-01-31 13:03:16 +0100

    Add project files.

---
+++ StarWarsModels/Rebel/IRebel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StarWarsModels
+{
+    public interface ICitizen
+    {
+        string Name { get; set; }
+        string Planet { get; set; }
+        DateTime RegistrationionDate { get; set; }
+    }
+}
---
+++ StarWarsModels/Rebel/Rebel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StarWarsModels
+{
+    public class Rebel : ICitizen
+    {
+        public string Name { get; set; }
+        public string Planet { get; set; }
+        public DateTime RegistrationionDate { get; set; }
+
+    }
+}
